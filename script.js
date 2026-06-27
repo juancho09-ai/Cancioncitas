32b02f4a4904f0345ba60e49a28ab2b42cf47f56
Juancho <juandddg89@gmail.com>
2026-06-27 11:15:40 -0500

    version 2.2

--- script.js
+++ script.js
@@ -2,170 +2,246 @@ const frases = [
 {
     frase: "Lograste comprender lo que ni yo entendí.",
     autor: "Milo J",
-    cancion: "M.A.I."
+    cancion: "M.A.I.",
+    link:"https://open.spotify.com/intl-es/track/35ttE4t8lQZA2vuCYDg4G7?si=4510db0adabe4998"
   },
+
   {
-    frase: "Contigo todo pesa menos.",
+    frase: "Sin ti, ¿qué soy yo?, un disfraz barato, contigo, soy yo, tú el motor del barco, si te tiras de un barranco, detrás tuyo salto.",
     autor: "Mora",
-    cancion: "Memorias"
+    cancion: "MEDIA LUNA",
+    link:"https://open.spotify.com/intl-es/track/7h3mCPlHYCPuSG93s7M3Xt?si=0292065f936844fa"
   },
   {
     frase: "Me diste vida, mucha más vida que el vino",
     autor: "Manuel Medrano",
-    cancion: "Bajo el Agua"
+    cancion: "Bajo el Agua",
+    link:"https://open.spotify.com/intl-es/track/03V0FZwKTJhXeW2zVNkyP7?si=be39dca6107c4eba"
   },
 
   {
     frase: "Te quiero más que ayer, y mucho menos que mañana",
     autor: "Eladio Carrión",
-    cancion: "TQMQA"
+    cancion: "TQMQA",
+    link:"https://open.spotify.com/intl-es/track/0Xm7WesKiHAAbLYluGqBZ7?si=74fb95f1f0154053"
   },
   {
     frase: "Con vos nunca hubo nada que dudar.",
     autor: "Milo J",
-    cancion: "Carencias de cordura"
+    cancion: "Carencias de cordura",
+    link: "https://open.spotify.com/intl-es/track/6QCTo3BaEdkEp0BbbNCVIt?si=5938e2556fdf49d0"
   },
   {
     frase: "Tus vueltas cautivaban las miradas de unos cuantos, tal vez, yo era uno más, pero deseé que fueras mía",
     autor: "Milo J",
-    cancion: "TUS VUELTAS"
+    cancion: "TUS VUELTAS",
+    link: "https://open.spotify.com/intl-es/track/7Ji2e01og2QOFBd8mnVF8L?si=b2d21f9a40384cba"
   },
   {
     frase: "Porque algún día te miraré a los ojos y sabremos que a pesar de la distancia, es como si entre nosotros, nada fuera a terminar.",
     autor: "Manuel Medrano",
-    cancion: "La Distancia"
+    cancion: "La Distancia",
+    link: "https://open.spotify.com/intl-es/track/0LpEr3qpmDQ1gEabnsQlGC?si=595da5d1ec0e4669"
   },
 
   {
     frase: "Darling, you look perfect tonight.",
     autor: "Ed Sheeran",
-    cancion: "Perfect"
+    cancion: "Perfect",
+    link: "https://open.spotify.com/intl-es/track/0tgVpDi06FyKpA1z0VMD4v?si=d11f5f37d03d466f"
   },
   {
     frase: "Oh, yeah, your skin and bones, turn into something beautiful.",
     autor: "Coldplay",
-    cancion: "Yellow"
+    cancion: "Yellow",
+    link: "https://open.spotify.com/intl-es/track/3AJwUDP919kvQ9QcozQPxg?si=7ad4c387c0da42c6"
   },
   {
     frase: "Love your curves and all your edges, all your perfect imperfections.",
     autor: "John Legend",
-    cancion: "All of Me"
+    cancion: "All of Me",
+    link: "https://open.spotify.com/intl-es/track/3U4isOIWM3VvDubwSI3y7a?si=022867776ec24bbf"
   },
     {
     frase: "Y me gusta esa cara tierna que pones cuando te frustras.",
     autor: "Milo J",
-    cancion: "Morocha"
+    cancion: "Morocha",
+    link: "https://open.spotify.com/intl-es/track/6U7yEVppOLVvWCKYnSAjC6?si=ba329914125f4c84"
   },
     {
     frase: "Can we always be this close, forever and ever?.",
     autor: "Taylor Swift",
-    cancion: "Lover"
+    cancion: "Lover",
+    link: "https://open.spotify.com/intl-es/track/1dGr1c8CrMLDpV6mPbImSI?si=84ec37ba41bb46fc"
   },
     {
     frase: "Y ya tú sabe' que lo mío es tuyo, oh-oh-oh, porque contigo es que yo fluyo, oh-oh-oh.",
     autor: "Mora",
-    cancion: "Tuyo"
+    cancion: "Tuyo",
+    link: "https://open.spotify.com/intl-es/track/4tQofG51E0juZBBVr6pral?si=b14699174e9e4999"
   },
     {
     frase: "Contigo to' es una aventura y a mí me gusta el peligro.",
     autor: "Mora",
-    cancion: "Reina"
+    cancion: "Reina",
+    link: "https://open.spotify.com/intl-es/track/0QRHOAeU8JRaVVn3UoZBih?si=0861518c6e2249ae"
   },
     {
     frase: "Nada e' normal contigo, baby, tú eres especial,To's mis dolores cómo los supiste anestesiar.",
     autor: "Eladio Carrión",
-    cancion: "Especial"
+    cancion: "Especial",
+    link: "https://open.spotify.com/intl-es/track/5sY7UZw6t8CYJNXp1K2y49?si=ef9356d148774635"
   },
     {
     frase: "Tal vez seas vos, quien me ayudará, si pierdo el control, y no puedo más.",
     autor: "Milo J",
-    cancion: "Tu Manta"
+    cancion: "Tu Manta",
+    link: "https://open.spotify.com/intl-es/track/0WEJDHCsiJyzyiJfHFGf63?si=8065e3fe78544321"
   },
     {
     frase: "Y lo peor es que yo te conozco tanto, que podría recordarte quien eres por si lo olvidas, y lo peor del caso es que de ti sé tanto, que podría dictar clases intensivas de tu vida.",
     autor: "Canserbero",
-    cancion: "Maquiavélico"
+    cancion: "Maquiavélico",
+    link: "https://open.spotify.com/intl-es/track/2RYOtMt9aj0p2mP5ofomFd?si=d7b05603e3cb45ea"
   },
     {
     frase: "Algo tan simple como que yo voy en ti y tú vas en mí.",
     autor: "Canserbero",
-    cancion: "Querer Querernos"
+    cancion: "Querer Querernos",
+    link: "https://open.spotify.com/intl-es/track/6d3q0F9VNtdxQUTVlRcet6?si=5b72dfae9bb54668"
   },
     {
     frase: "Miras con tanta dulzura, hermosa criatura, tú, mi bello ángel que cayó del cielo.",
     autor: "Natanael Cano",
-    cancion: "Mi bello ángel"
+    cancion: "Mi bello ángel",
+    link: "https://open.spotify.com/intl-es/track/4t46soaqA758rbukTO5pp1?si=8881fb2c199f4890"
   },
     {
     frase: "Y también yo ando solo, y soy solo pa' ti, corazón.",
     autor: "Tito Double P",
-    cancion: "La Loba"
+    cancion: "La Loba",
+    link: "https://open.spotify.com/intl-es/track/37JYh8Iyhtnb2MGrdig3oK?si=b97f327f2e9246a0"
   },
     {
     frase: "Por esos ojos, yo robo y mato.",
     autor: "Fuerza Regida",
-    cancion: "Por esos ojos"
+    cancion: "Por esos ojos",
+    link: "https://open.spotify.com/intl-es/track/5jHozZMWbrExAFni95mvvH?si=c17fc986e75e4c80"
   },
     {
     frase: "Tú, el aire que respiro yo, y la luz de la Luna en el mar, la garganta que ansio mojar, que temo ahogar de amor.",
     autor: "Rodrigo Amarante",
-    cancion: "Tuyo(Narcos Theme)"
+    cancion: "Tuyo(Narcos Theme)",
+    link: "https://open.spotify.com/intl-es/track/6g2BiiVQqY5v1S4HIrM54F?si=91d1ada765d242c7"
   },
     {
     frase: "Prefiero tus labio' a cualquier premio, prefiero tu boca que un número uno.",
     autor: "Alvaro Díaz",
-    cancion: "YOKO"
+    cancion: "YOKO",
+    link: "https://open.spotify.com/intl-es/track/6dlZryuz7NIz20faRoZV4k?si=480f8ad096bb4a63"
   },
     {
     frase: "Y si pudiera escoger mis sueño', yo soñaría contigo, ná' más.",
     autor: "Alvaro Díaz",
-    cancion: "NO PODEMOS SER AMIGOS."
+    cancion: "NO PODEMOS SER AMIGOS.",
+    link: "https://open.spotify.com/intl-es/track/4T31PPb8DMhzTp7kUZ3U42?si=5baa8ae45a914d05"
   },
     {
     frase: "Si estoy cerca de ti, se me acelera el pulso, siempre caigo easy.",
     autor: "Mora",
-    cancion: "AURORA"
+    cancion: "AURORA",
+    link: "https://open.spotify.com/intl-es/track/4UHXMDRuuL07rnFEmSgBEG?si=31d9767c68884ad4"
   },
     {
     frase: "En tus brazos, siempre en tus brazos.",
     autor: "Milo J",
-    cancion: "Ama de mi sol"
+    cancion: "Ama de mi sol",
+    link: "https://open.spotify.com/intl-es/track/4GKm1QaEr1tqJwUM0EsUl3?si=5dae466a6b104b78"
   },
     {
     frase: "I don't wanna look at anything else now that I saw you.",
     autor: "Taylor Swift",
-    cancion: "Daylight"
+    cancion: "Daylight",
+    link: "https://open.spotify.com/intl-es/track/1fzAuUVbzlhZ1lJAx9PtY6?si=bcbdacfd92524a15"
   },
     {
     frase: "And you've got a smile that could light up this whole town.",
     autor: "Taylor Swift",
-    cancion: "You Belong With Me"
+    cancion: "You Belong With Me",
+    link: "https://open.spotify.com/intl-es/track/1qrpoAMXodY6895hGKoUpA?si=d29a582dccdb47a9"
   },
     {
     frase: "Eres la mujer, (eres la mujer) que tiene todo el derecho sobre mí.",
     autor: "Manuel Medrano",
-    cancion: "La Mujer Que Bota Fuego"
+    cancion: "La Mujer Que Bota Fuego",
+    link: "https://open.spotify.com/intl-es/track/236RkCUAucOj7QXfeVWOfA?si=209c17f962254cd6"
   },
     {
     frase: "Eres la única que me pone a bailar, que me pone a sudar, la única que me pone la cabeza a andar, el corazón a palpitar.",
     autor: "Manuel Medrano",
-    cancion: "Donde Nadie Pueda Ir"
+    cancion: "Donde Nadie Pueda Ir",
+    link: "https://open.spotify.com/intl-es/track/05xgON7xWm3yN1yarMHdBv?si=4d53dec9a8ab4dab"
   },
     {
     frase: "Eres mi canción favoritaYo quisiera darte las constelaciones, Más millones de caricias en un manantial.",
     autor: "Micro TDH",
-    cancion: "Cafuné"
+    cancion: "Cafuné",
+    link: "https://open.spotify.com/intl-es/track/4DhTBCvoemYivBVHkk7RKI?si=23583d1a0a9f49d5"
   },
     {
     frase: "Soy capaz de lo incapaz por ti mi cielo.",
     autor: "Romeo Santos",
-    cancion: "Mi Santa"
+    cancion: "Mi Santa",
+    link: "https://open.spotify.com/intl-es/track/4EbAftNM732UGLF8gmIIsX?si=ee25cb0a54fb4056"
   },
     {
-    frase: "Si yo fuera El Hombre Araña por un beso, cien pies de altura escalo sin esfuerzo a tu balcón.",
+    frase: " Si yo fuera El Hombre Araña por un beso, cien pies de altura escalo sin esfuerzo a tu balcón.",
     autor: "Romeo Santos",
-    cancion: "Héroe Favorito"
-  }
+    cancion: "Héroe Favorito",
+    link: "https://open.spotify.com/intl-es/track/6BdAwMv1YorLfkBWlE493X?si=4304edb23cad42af"
+  },
+      {
+    frase: "Labios hermosos, su rostro espectacular.",
+    autor: "Junior H",
+    cancion: "Ella",
+    link: "https://open.spotify.com/intl-es/track/0tKl8ZVM6xJkhznvJAS3Nq?si=7b3e56bcaceb4e2f"
+  },
+      {
+    frase: "Yo voy a amarte, todos los días, todas las noches.",
+    autor: "Junior H",
+    cancion: "Yo Voy a Amarte",
+    link: "https://open.spotify.com/intl-es/track/2DQWbH7mMQYRuftnYPlfqQ?si=cfeeb6a134f9430c"
+  },
+      {
+    frase: "Todas esas caricias, que me llenan de vida, nací para amarte.",
+    autor: "Junior H",
+    cancion: "Naci Para Amarte",
+    link: "https://open.spotify.com/intl-es/track/0x7VKOWpEJflK0gvy3hTmk?si=ed2c8dbdb3434cf4"
+  },
+      {
+    frase: "Preso, de la cárcel de tus besos.",
+    autor: "Jose Jose",
+    cancion: "Preso",
+    link: "https://open.spotify.com/intl-es/track/6HlIwEASZhw0cO2YtzxQ8f?si=100f16b335204c86"
+  },
+        {
+    frase: "Quiero perderme contigo, como se pierde el horizonte.",
+    autor: "Jose Jose",
+    cancion: "Quiero Perderme Contigo",
+    link: "https://open.spotify.com/intl-es/track/1nbte2bCwsxqioAhzo68aE?si=c5d60dbfa6d14fe0"
+  },
+        {
+    frase: "Y es que tú y yo, fuimos hechos para estar, solo tú y yo, y nadie más.",
+    autor: "Jose Jose",
+    cancion: "Solo tu y yo",
+    link: "https://open.spotify.com/intl-es/track/7wbkgofauDh34nVsBZvkty?si=eafa5e538af04e18"
+  },
+        {
+    frase: "Tu cuerpo es poesía, por tus beso', mataría.",
+    autor: "Alvaro Díaz",
+    cancion: "Reina Pepiada",
+    link: "https://open.spotify.com/intl-es/track/03V0FZwKTJhXeW2zVNkyP7?si=d54040f9f833452a"
+  },
 ];
 
 const abrirBtn = document.getElementById("abrirBtn");
@@ -192,7 +268,11 @@ function mostrarFrase() {
   const random = Math.floor(Math.random() * frases.length);
   fraseTexto.textContent = `"${frases[random].frase}"`;
   autorTexto.textContent = `— ${frases[random].autor}`;
-  cancionTexto.textContent = frases[random].cancion;
+  cancionTexto.innerHTML = `
+  <a href="${frases[random].link}" target="_blank">
+    🎵 ${frases[random].cancion}
+  </a>
+`;
 }
 
 function resizeCanvas() {
